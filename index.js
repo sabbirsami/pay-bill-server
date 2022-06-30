6483fd7c6b62c6fc43733fa0c5b9fa697d51172a
SabbirSami <smd71430@gmail.com>
2022-06-30 21:01:36 +0600

    get all registration user

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.up3hj.mongodb.net/?retryWrites=true&w=majority`;
 
 app.use(cors());
@@ -19,6 +19,7 @@ async function run() {
     try {
         await client.connect();
         const billCollection = client.db("payBill").collection("bills");
+        const userCollection = client.db("payBill").collection("registration");
 
         // GET ALL BILL FROM DATABASE
         app.get("/bills", async (req, res) => {
@@ -32,6 +33,10 @@ async function run() {
                 .toArray();
             res.send(result);
         });
+        app.get("/bills", async (req, res) => {
+            const result = await billCollection.find().toArray();
+            res.send(result);
+        });
 
         // GET TOTAL BILL
         app.get("/billsCount", async (req, res) => {
@@ -39,9 +44,33 @@ async function run() {
             res.send({ count });
         });
 
-        //LOAD DATA USE QUERY
-        app.get("/bills", async (req, res) => {
-            const query = {};
+        //LOAD REGISTRATION DATA
+        app.get("/registration", async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.post("/registration", async (req, res) => {
+            const newUser = req.body;
+            console.log(newUser);
+            // const name = req.body.name;
+            // const email = req.body.email;
+            // const password = par;
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        });
+
+        app.get("/registration", async (req, res) => {
+            const query = req.body;
+            console.log(query);
+            const result = await userCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get("/registration/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            res.send(user);
         });
 
         // GET DATA FROM CLIENT SIDE
@@ -49,6 +78,21 @@ async function run() {
             const newBill = req.body;
             console.log(newBill);
             const result = await billCollection.insertOne(newBill);
+            res.send(result);
+        });
+        app.get("/bills/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await billCollection.findOne(query);
+            res.send(result);
+        });
+
+        // DELETE BILL BY ID
+        app.delete("/bills/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await billCollection.deleteOne(query);
+            console.log(id);
             res.send(result);
         });
     } finally {
